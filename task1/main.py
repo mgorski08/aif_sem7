675abc2d39702a27eb1e7b7b55d14725f29dca2a
Michał Górski <mgorski.08@ittcertex.pl>
2020-10-22 20:58:00 +0200

    task 1 finished

--- task1/main.py
+++ task1/main.py
@@ -3,9 +3,27 @@ from task1.visual import Visual
 import numpy as np
 
 class0 = []
+class1 = []
+
 
 def refresh():
-    vis.draw_points(generate_mode(0, 0, 0.02, 50000), "green")
+    class0.clear()
+    class1.clear()
+    c0_modes = int(vis.modes_c0_entry.get())
+    c1_modes = int(vis.modes_c1_entry.get())
+    c0_ppm = int(vis.p_per_mode_c0_entry.get())
+    c1_ppm = int(vis.p_per_mode_c1_entry.get())
+    c0_spread = float(vis.spread_c0_entry.get())
+    c1_spread = float(vis.spread_c1_entry.get())
+
+    for _ in range(c0_modes):
+        class0.extend(generate_mode(np.random.random(), np.random.random(), c0_spread, c0_ppm))
+    for _ in range(c1_modes):
+        class1.extend(generate_mode(np.random.random(), np.random.random(), c1_spread, c1_ppm))
+
+    vis.canvas.delete("all")
+    vis.draw_points(class0, "blue")
+    vis.draw_points(class1, "red")
 
 
 def generate_mode(x, y, std_dev, n):
--- task1/visual.py
+++ task1/visual.py
@@ -8,19 +8,31 @@ class Visual:
         self.size = size
         self.root = tkinter.Tk()
 
-        self.modes_c0_label = tkinter.Label(self.root, text="Number of modes (class 0)")
+        tkinter.Label(self.root, text="Number of modes").grid(row=0, column=1)
+        tkinter.Label(self.root, text="Points per mode").grid(row=0, column=2)
+        tkinter.Label(self.root,          text="Spread").grid(row=0, column=3)
+
+        tkinter.Label(self.root,         text="Class 0").grid(row=1, column=0)
+        tkinter.Label(self.root,         text="Class 1").grid(row=2, column=0)
+
         self.modes_c0_entry = tkinter.Entry(self.root)
-        self.modes_c1_label = tkinter.Label(self.root, text="Number of modes (class 1)")
         self.modes_c1_entry = tkinter.Entry(self.root)
+        self.p_per_mode_c0_entry = tkinter.Entry(self.root)
+        self.p_per_mode_c1_entry = tkinter.Entry(self.root)
+        self.spread_c0_entry = tkinter.Entry(self.root)
+        self.spread_c1_entry = tkinter.Entry(self.root)
+
         self.button = tkinter.Button(self.root, text='Refresh', command=self.refresh)
         self.canvas = tkinter.Canvas(self.root, width=size, height=size)
 
-        self.modes_c0_label.grid(row=0, column=0)
-        self.modes_c0_entry.grid(row=0, column=1)
-        self.modes_c1_label.grid(row=1, column=0)
-        self.modes_c1_entry.grid(row=1, column=1)
-        self.button.grid(row=2, column=0, columnspan=2)
-        self.canvas.grid(row=3, column=0, columnspan=2)
+        self.modes_c0_entry.grid(row=1, column=1)
+        self.modes_c1_entry.grid(row=2, column=1)
+        self.p_per_mode_c0_entry.grid(row=1, column=2)
+        self.p_per_mode_c1_entry.grid(row=2, column=2)
+        self.spread_c0_entry.grid(row=1, column=3)
+        self.spread_c1_entry.grid(row=2, column=3)
+        self.button.grid(row=3, column=0, columnspan=4)
+        self.canvas.grid(row=4, column=0, columnspan=4)
 
     def draw_point_raw(self, x, y, fill):
         self.canvas.create_oval(x - 1, y - 1, x + 2, y + 2, fill=fill, width=0)
